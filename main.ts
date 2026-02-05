9cc6e14e8ecad70a3dcdaa38c1d993aa237cde7a
georg2hh-gif <georg_2_hh@gmx.de>
2026-02-05 12:09:48 +0100

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,20 @@
+function sonar_sensor () {
+    if (JoyCar.sonar() >= 50) {
+        return 0
+    } else if (JoyCar.sonar() < 50) {
+        return 1
+    } else {
+        return 2
+    }
+}
+JoyCar.initJoyCar(RevisionMainboard.OnepThree)
+JoyCar.initController(ControllerType.Calliope)
 basic.forever(function () {
-	
+    if (sonar_sensor() == 1) {
+        JoyCar.light(ToggleSwitch.Off)
+        JoyCar.hazardlights(ToggleSwitch.On)
+    } else if (sonar_sensor() == 0) {
+        JoyCar.light(ToggleSwitch.On)
+        JoyCar.hazardlights(ToggleSwitch.Off)
+    }
 })
