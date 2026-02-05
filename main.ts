5c7b4a76b09aa31aaecaa3e078e89dced80104a9
georg2hh-gif <georg_2_hh@gmx.de>
2026-02-05 12:22:55 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,20 +1,38 @@
-function sonar_sensor () {
-    if (JoyCar.sonar() >= 50) {
-        return 0
-    } else if (JoyCar.sonar() < 50) {
-        return 1
-    } else {
-        return 2
-    }
-}
 JoyCar.initJoyCar(RevisionMainboard.OnepThree)
 JoyCar.initController(ControllerType.Calliope)
 basic.forever(function () {
-    if (sonar_sensor() == 1) {
-        JoyCar.light(ToggleSwitch.Off)
-        JoyCar.hazardlights(ToggleSwitch.On)
-    } else if (sonar_sensor() == 0) {
-        JoyCar.light(ToggleSwitch.On)
-        JoyCar.hazardlights(ToggleSwitch.Off)
-    }
+    JoyCar.drive(FRLRDirection.Forward, 100)
+    basic.pause(2000)
+    JoyCar.drive(FRLRDirection.Reverse, 100)
+    basic.pause(2000)
+    JoyCar.turn(
+    FRDirection.Forward,
+    LRDirection.Left,
+    100,
+    0
+    )
+    basic.pause(2000)
+    JoyCar.turn(
+    FRDirection.Forward,
+    LRDirection.Right,
+    100,
+    0
+    )
+    basic.pause(2000)
+    JoyCar.stop(StopIntensity.Intense)
+    basic.pause(2000)
+    JoyCar.turn(
+    FRDirection.Forward,
+    LRDirection.Left,
+    50,
+    4
+    )
+    basic.pause(1000)
+    JoyCar.turn(
+    FRDirection.Reverse,
+    LRDirection.Right,
+    50,
+    2
+    )
+    basic.pause(1000)
 })
